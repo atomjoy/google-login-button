c59b69abb93707d69904d62cc532a90ab79a1b23
atomjoy <atomjoy.official@gmail.com>
2023-04-27 11:37:56 +0200

    Vue custom goole login button

---
+++ sample/GoogleLoginButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { getGoogleLoginButtonUrl } from './getGoogleLoginButtonUrl.js'
+
+const from = '/login'
+</script>
+
+<template>
+<a :href="getGoogleLoginButtonUrl(from)" class="google-login-button">
+  <img src="https://developers.google.com/static/identity/images/g-logo.png" alt="Google Logo" />
+  <span>Google Sign In</span>
+</a>
+</template>
+
+<style>
+.google-login-button {
+  color: #fff;
+  background: #07f;
+  padding: 10px 20px;
+  border-radius: 6px;
+}
+</style>
