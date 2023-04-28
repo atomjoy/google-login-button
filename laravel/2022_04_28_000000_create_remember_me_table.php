9694f66e8f8d5df95350f5f51613b3954fbca1bc
atomjoy <atomjoy.official@gmail.com>
2023-04-28 10:17:26 +0200

    Login Remember_me token migration

---
+++ laravel/2022_04_28_000000_create_remember_me_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::table('users', function ($table) {
+			if (!Schema::hasColumn('users', 'remember_token')) {
+				$table->rememberToken(); // remember_token
+			}
+
+			if (!Schema::hasColumn('users', 'oauth_provider')) {
+				$table->string('oauth_provider', 100)->default('local');
+			}
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::table('users', function ($table) {
+			$table->dropColumn(['remember_token', 'oauth_provider']);
+		});
+	}
+};
